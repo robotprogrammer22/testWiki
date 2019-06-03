05df4d4affec2ef8ebbd8f897312ac4e825b3aab
robotprogrammer22 <46390033+robotprogrammer22@users.noreply.github.com>
2019-06-03 09:23:17 -0700

    updating image links

--- themes/astro/javascripts/theme.js
+++ themes/astro/javascripts/theme.js
@@ -67,35 +67,35 @@ function loadFooter() {
 var footerHTML = '<div class="icons projects black scroll-wrapper">' +
         '<div class="scroll">' +
 	'<a title="Integrated Software for Imagers and Spectrometers" href="http://isis.astrogeology.usgs.gov" class="icon">' +
-	'<img alt="ISIS Logo" height="112" width="112" src="/themes/astro/images/logos/isis_2x.jpg"/>' +
+	'<img alt="ISIS Logo" height="112" width="112" src="themes/astro/images/logos/isis_2x.jpg"/>' +
 	'<span class="label">ISIS</span>' +
 	'</a>' +
 	'<a title="Gazetteer of Planetary Nomenclature" href="http://planetarynames.wr.usgs.gov" class="icon">' +
-	'<img alt="Nomenclature Logo" height="112" width="112" src="/themes/astro/images/logos/nomenclature_2x.jpg"/>' +
+	'<img alt="Nomenclature Logo" height="112" width="112" src="themes/astro/images/logos/nomenclature_2x.jpg"/>' +
 	'<span class="label">Planetary Nomenclature</span>' +
 	'</a>' +
 	'<a title="Map-a-Planet" href="http://www.mapaplanet.org" class="icon">' +
-	'<img alt="Map-a-Planet Logo" height="112" width="112" src="/themes/astro/images/logos/map_a_planet_2x.jpg"/>' +
+	'<img alt="Map-a-Planet Logo" height="112" width="112" src="themes/astro/images/logos/map_a_planet_2x.jpg"/>' +
 	'<span class="label">Map-a-Planet</span>' +
 	'</a>' +
 	'<a title="PDS Imaging Node" href="http://astrogeology.usgs.gov/facilities/imaging-node-of-nasa-planetary-data-system-pds" class="icon">' +
-	'<img alt="PDS Logo" height="112" src="/themes/astro/images/logos/pds_logo-black-web.png"/>' +
+	'<img alt="PDS Logo" height="112" src="themes/astro/images/logos/pds_logo-black-web.png"/>' +
 	'<span class="label">PDS Imaging Node</span>' +
 	'</a>' +
 	'<a title="Regional Planetary Image Facility" href="http://astrogeology.usgs.gov/rpif" class="icon">' +
-	'<img alt="RPIF Logo" height="112" width="112" src="/themes/astro/images/logos/rpif_2x.jpg"/>' +
+	'<img alt="RPIF Logo" height="112" width="112" src="themes/astro/images/logos/rpif_2x.jpg"/>' +
 	'<span class="label">RPIF</span>' +
 	'</a>' +
 	'<a title="Photogrammetry Guest Facility" href="http://astrogeology.usgs.gov/facilities/photogrammetry-guest-facility" class="icon">' +
-	'<img alt="Photogrammetry Guest Faciltiy Logo" height="112" width="112" src="/themes/astro/images/logos/photogrammetry_2x.jpg"/>' +
+	'<img alt="Photogrammetry Guest Faciltiy Logo" height="112" width="112" src="themes/astro/images/logos/photogrammetry_2x.jpg"/>' +
 	'<span class="label">Photogrammetry Guest Facility</span>' +
 	'</a>' +
 	'<a title="Planetary Image Locator Tool" href="http://pilot.wr.usgs.gov" class="icon">' +
-	'<img alt="Pilot Logo" height="112" width="112" src="/themes/astro/images/logos/pilot_2x.jpg"/>' +
+	'<img alt="Pilot Logo" height="112" width="112" src="themes/astro/images/logos/pilot_2x.jpg"/>' +
 	'<span class="label">PILOT</span>' +
 	'</a>' +
 	'<a title="Mapping, Remote-sensing, Cartography, Technology and Research GIS Lab" href="http://astrogeology.usgs.gov/facilities/mrctr" class="icon">' +
-	'<img alt="MRCTR GIS Lab Logo" height="112" width="112" src="/themes/astro/images/logos/mrctr_2x.jpg"/>' +
+	'<img alt="MRCTR GIS Lab Logo" height="112" width="112" src="themes/astro/images/logos/mrctr_2x.jpg"/>' +
 	'<span class="label">MRCTR GIS Lab</span>' +
 	'</a>' +
    '</div></div>';
